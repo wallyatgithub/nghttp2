9fdfca74c6674af678219997783b197ea357eb16
Wally Zhao <wallyzhao@gmail.com>
2021-08-02 23:49:49 +0800

    wrap up

--- examples/h2server_Request_Match.h
+++ examples/h2server_Request_Match.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <list>
 #include <map>
+#include <set>
 #include "h2server_Config_Schema.h"
 #include "h2server_Resonse.h"
 #include "h2server_Message.h"
@@ -13,118 +14,162 @@
 
 using rapidjson;
 
-struct ci_less
-{
-  // case-independent (ci) compare_less binary function
-  struct nocase_compare
-  {
-    bool operator() (const unsigned char& c1, const unsigned char& c2) const {
-        return tolower (c1) < tolower (c2);
-    }
-  };
-  bool operator() (const std::string & s1, const std::string & s2) const {
-    return std::lexicographical_compare
-      (s1.begin (), s1.end (),   // source range
-      s2.begin (), s2.end (),   // dest range
-      nocase_compare ());  // comparison
-  }
-};
-
-enum Match_Type {
-  EQUALS_TO = 0,
-  START_WITH,
-  END_WITH,
-  CONTAINS
-};
-
-std::map<std::string, Match_Type> string_to_match_type{{"EqualsTo", EQUALS_TO}, {"StartsWith", START_WITH}, {"EndsWith", END_WITH}, {"Contains", CONTAINS}};
-
-bool match(const std::string& subject, Match_Type verb, const std::string& object)
-{
-  switch (verb)
-  {
-      case EQUALS_TO:
-      {
-          return (subject == object);
-      }
-      case START_WITH:
-      {
-          return (subject.find(object) == 0);
-      }
-      case END_WITH:
-      {
-          return (subject.size() >= object.size() && 0 == subject.compare(subject.size()- object.size(), object.size(), object));
-      }
-      case END_WITH:
-      {
-          (subject.find(object) != std::string::npos);
-      }
-  }
-  return false;
-}
-
 class Match_Interface {
 public:
   std::list<Match_Interface*> next_matches;
   H2Server_Response* response_to_return;
 };
 
-class Match_Header: public Match_Interface{
+class Match_Rule: public Match_Interface{
 public:
   Match_Type match_type;
   std::string header_name;
+  std::string json_pointer;
   std::string object;
-  Match_Header(const Schema_Path_Match& path_match)
+  Match_Rule(const Schema_Path_Match& path_match)
   {
       object = path_match.input;
       header_name = ":path";
+      json_pointer = "";
       match_type = string_to_match_type[path_match.matchType];
   }
-  bool match(const std::string& subject)
-  {
-    return match(subject, match_type, object);
-  }
-};
-class Match_Payload: public Match_Interface{
-public:
-  Match_Type match_type;
-  std::string json_pointer;
-  std::string object;
-  Match_Payload(const Schema_Payload_Match& payload_match)
+  Match_Rule(const Schema_Payload_Match& payload_match)
   {
       object = payload_match.input;
-      match_type = string_to_match_type[payload_match.matchType];
+      header_name = "";
       json_pointer = payload_match.jsonPointer;
+      match_type = string_to_match_type[path_match.matchType];
   }
+
+  enum Match_Type {
+    EQUALS_TO = 0,
+    START_WITH,
+    END_WITH,
+    CONTAINS
+  };
+  
+  std::map<std::string, Match_Type> string_to_match_type{{"EqualsTo", EQUALS_TO}, {"StartsWith", START_WITH}, {"EndsWith", END_WITH}, {"Contains", CONTAINS}};
+  
+  bool match(const std::string& subject, Match_Type verb, const std::string& object)
+  {
+      switch (verb)
+      {
+          case EQUALS_TO:
+          {
+              return (subject == object);
+          }
+          case START_WITH:
+          {
+              return (subject.find(object) == 0);
+          }
+          case END_WITH:
+          {
+              return (subject.size() >= object.size() && 0 == subject.compare(subject.size()- object.size(), object.size(), object));
+          }
+          case END_WITH:
+          {
+              (subject.find(object) != std::string::npos);
+          }
+      }
+      return false;
+  }
+
+  bool match(const std::string& subject)
+  {
+      return match(subject, match_type, object);
+  }
+  
   bool match(const rapidjson::Document::Document& d)
   {
-    return match(getJsonPointerValue(d, json_pointer), match_type, object);
+      return match(getJsonPointerValue(d, json_pointer), match_type, object);
   }
+
+  bool match(const H2Server_Request_Message& request)
+  {
+      return (header_name.size() ? match(request.path) : match(request.json_payload));
+  }
+
+  bool operator<(const Match_Header& rhs) const 
+  {
+     if (!header_name.empty() && rhs.header_name.empty())
+     {
+        return true;
+     }
+     else if (header_name.empty() && !rhs.header_name.empty())
+     {
+        return false;
+     }
+
+     if (match_type < rhs.match_type)
+     {
+        return true;
+     }
+     if (header_name < rhs.header_name)
+     {
+        return true;
+     }
+     if (json_pointer < rhs.json_pointer)
+     {
+        return true;
+     }
+     if (object < rhs.object)
+     {
+        return true;
+     }
+     return false;
+  }
+
 };
 
 class H2Server_Request {
 public:
-  Match_Header match_header;
-  std::vector<Match_Payload> match_payload;
+  std::set<Match_Rule> match_rules;
   H2Server_Request(const Schema_Request_Match& request_match):
-  match_header(request_match.path_match)
   {
+      match_rules.emplace_back(Match_Rule(request_match.path_match));
       for (auto& schema_payload_match: request_match.payload_match)
-      match_payload.emplace_back(Match_Payload(schema_payload_match));
+      {
+          match_rules.emplace(Match_Rule(schema_payload_match));
+      }
   }
   bool match(const H2Server_Request_Message& request)
   {
-      if (!match_header.match(request.path))
+      for (auto& match: match_rules)
       {
-          return false;
-      }
-      for (auto& payload_match: match_payload)
-      {
-          if (!payload_match.match(request.json_payload))
+          if (!match.match(request))
           {
               return false;
           }
       }
+  }
+  bool operator<(const H2Server_Request& rhs) const 
+  {
+      if (match_rules.size() < rhs.rhs.size())
+      {
+          return true;
+      }
+      else if (match_rules.size() > rhs.match_rules.size())
+      {
+          return false;
+      }
+      else
+      {
+          auto& match = match_rules.begin();
+          auto& other_match = rhs.match_rules.begin();
+          while (match != match_rules.end() && other_match != rhs.match_rules.end())
+          {
+              if (*match < *other_match)
+              {
+                  return true;
+              }
+              else
+              {
+                  match++;
+                  other_match++;
+              }
+          }
+      }
+      return false;
   }
 };
 
@@ -140,5 +185,27 @@ public:
   }
 };
 
+Match_Interface* find_or_insert_into_match_tree(std::vector<Match_Interface*>& tree, )
+{
+    
+}
+
+
+void build_match_tree(const H2Server_Config_Schema& config_schema)
+{
+    std::vector<Match_Interface*> tree;
+
+    std::vector<H2Server_Service> services;
+
+    for (auto& service_schema: config_schema.service)
+    {
+        services.emplace_back(H2Server_Service(service_schema));
+    }
+
+    for (auto& service: services)
+    {
+        
+    }
+}
 
 #endif
